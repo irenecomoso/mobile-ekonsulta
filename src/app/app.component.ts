8c5cf58bddf172261204be1623cf2714318ea559
irene <irenecomoso@gmail.com>
2021-12-13 01:51:39 +0800

    patient validate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,14 +15,14 @@ export class AppComponent {
   constructor(private router: Router,private menu: MenuController) {}
 
   click(){
-    //window.location.href='/patient-profile';
-    this.router.navigate(['/patient-profile']);
+    window.location.href='/patient-profile';
+    //this.router.navigate(['/patient-profile']);
     this.menu.close();
   }
 
   click2(){
-    //window.location.href='/doctor-profile';
-    this.router.navigate(['/doctor-profile']);
+    window.location.href='/doctor-profile';
+    //this.router.navigate(['/doctor-profile']);
     this.menu.close();
   }
 
