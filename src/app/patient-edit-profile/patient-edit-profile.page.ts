26210a6a17bdce80bcd41ce0083f594405516ad8
irene <irenecomoso@gmail.com>
2021-12-13 07:46:12 +0800

    updt

--- src/app/patient-edit-profile/patient-edit-profile.page.ts
+++ src/app/patient-edit-profile/patient-edit-profile.page.ts
@@ -160,7 +160,8 @@ export class PatientEditProfilePage implements OnInit {
       this.userservice.update_user(this.userID,frm).then(()=>{
       console.log('patient Updated!');
       this.ngOnInit();
-      window.location.href='/patient-profile';
+      //window.location.href='/patient-profile';
+      this.router.navigate(['/patient-profile']);
     })
   }
   data(frm){
